1b55ce1617ff120704b228baf94cdf32925147a5
leonardo rodrigues <leo-feitosa@live.com>
2022-12-06 17:50:00 -0300

    Componente lista

--- src/pages/Cesta/index.js
+++ src/pages/Cesta/index.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 
 import Topo from "./components/Topo";
 import Detalhes from "./components/Detalhes";
+import Itens from "./components/Itens";
 
 
-export default function Cesta({ topo, detalhes }) {
+export default function Cesta({ topo, detalhes, itens }) {
   return (
-    <>
+    <ScrollView>
       <Topo {...topo} />
       
       <View style={estilos.cesta}>
         <Detalhes {...detalhes}/>
+        <Itens {...itens}/>
       </View>
-    </>
+    </ScrollView>
   )
 }
 
